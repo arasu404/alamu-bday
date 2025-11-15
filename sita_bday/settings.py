a358ad81c89c5c8c5f3fdd651049e02d4925acd2
arasu <perarasukounder@gmail.com>
2025-11-16 02:08:32 +0530

    Update ALLOWED_HOSTS for production deployment

--- sita_bday/settings.py
+++ sita_bday/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'replace-this-with-a-secure-key'   # CHANGE before production
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://alamu.onrender.com/]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
